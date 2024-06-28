bf02354f6f2742ab411f53e59ce7ebe5731002d2
pothamsettinagaraju <167834888+pothamsettinagaraju@users.noreply.github.com>
2024-06-28 11:37:53 +0530

    Update list_projects.py

--- Day_14/examples/list_projects.py
+++ Day_14/examples/list_projects.py
@@ -6,7 +6,7 @@ import json
 
 url = "https://nagarajupothamsetti.atlassian.net/rest/api/3/project"
 
-API_TOKEN=""
+API_TOKEN="ATATT3xFfGF0Ld9D4z072PLzGAxpU-mJzb12TdYtUgvLQMpHvkuRp0K3bHIzT36uLjcdlrRx2mHwB1386KcQF2T-tqAqbNAFFln2RvZJ4PYo92ud9IyotGr7hXmLyTYfJKH2vNlNWHsGCN_omhdBKU9_v4v3AMhRRyHLREIisYE4uA0A8Oz1orQ=655D59C3"
 
 auth = HTTPBasicAuth("nagaraju.pothamsetti@gmail.com", API_TOKEN)
 
